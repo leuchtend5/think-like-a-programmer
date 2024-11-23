bf147567c3b7bb2fb5f057580efc5cacf57589be
leuchtend5 <zxcmj1@gmail.com>
2024-11-24 01:15:06 +0700

    feat: add exercise 2-2

---
+++ chapter-2/exercise-2-2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using std::cout;
+
+int main() {
+  for (int row = 1; row <= 8; row++) {
+    int pivot = 4;
+    int emptySpace = (row > pivot) ? row - 1 : row;
+
+    for (int hasNum = 1; hasNum <= abs(pivot - emptySpace); hasNum++) {
+      cout << " ";
+    }
+
+    for (int hasNum = 1; hasNum <= 8 - (abs(pivot - emptySpace) * 2);
+         hasNum++) {
+      cout << "#";
+    }
+
+    cout << "\n";
+  }
+}
